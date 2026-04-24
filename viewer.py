3c8b21193500883bf83ae1cfde059f3304f2fc31
greg <egreglee@gmail.com>
2026-04-24 11:52:31 -0700

    viewer.py clean up logic and output formats

--- viewer.py
+++ viewer.py
@@ -17,54 +17,56 @@ def main():
     if args.zero_g:
         model.opt.gravity[:] = 0
 
-    watch_site = "jaw_grasp" if args.site is None else args.site
-
+    watch = "moving_jaw_grasp" if args.site is None else args.site
+    alt = "moving_jaw_grasp"
     with mujoco.viewer.launch_passive(model, data) as viewer:
-        site_prev = np.zeros((3,))
-        fj_site_prev = np.zeros((3,))
+        watch_site_prev = np.zeros((3,))
+        alt_site_prev = np.zeros((3,))
         settled = False  # for convergence
         while viewer.is_running():
             mujoco.mj_step(model, data)
 
-            # monitor jaw target
-            site_world_pos = data.site(watch_site + "_site").xpos
-            fj_site_world_pos = data.site("fixed_jaw_grasp" + "_site").xpos
+            # monitor watch site, usually moving_jaw target
+            watch_site_world_pos = data.site(watch + "_site").xpos
+            alt_site_world_pos = data.site(alt + "_site").xpos
 
             # on startup or after a control action in the viewer, allclose should be false.
             # when the simulation as settled down, i.e. the targeted site stops moving,
             # allclose will be true.
-            if not np.allclose(site_prev, site_world_pos, atol=1e-4, rtol=1e-4) or \
-               not np.allclose(fj_site_prev, fj_site_world_pos, atol=1e-6, rtol=1e-4):
+            if not np.allclose(watch_site_prev, watch_site_world_pos, atol=1e-4, rtol=1e-4) or \
+               not np.allclose(alt_site_prev, alt_site_world_pos, atol=1e-6, rtol=1e-4):
                 settled = False
             else:
                 if not settled:
                     # up until now the sim hadn't settled, so evaluate this state.
-                    print(f"{watch_site}: {site_world_pos}")
-                    for n in ["base", "rotation", "pitch", "elbow", "wrist_pitch", "wrist_roll", "jaw", "moving_jaw_grasp", "fixed_jaw_grasp", "jaw_grasp"]:
+                    sites = ["base", "rotation", "pitch", "elbow", "wrist_pitch", "wrist_roll", "jaw", "moving_jaw_grasp", "fixed_jaw_grasp", "jaw_grasp"]
+                    sites_xpos = {}
+                    for n in sites:
                         n_ = n + "_site"
                         swp = data.site(n_).xpos
-                        print(f"{n_:22}: {swp}")
+                        sites_xpos[n] = swp
+                        print(f"{n_:22}: {swp[0]:8.5f}, {swp[1]:8.5f}, {swp[2]:8.5f}")
 
                     # check forward kinetics models
                     qpos = data.qpos.copy()
-                    print(f"qpos {qpos}")
-                    all = d3.threed_fk(qpos, False)
-                    pfw = all['jaw_grasp']
-                    print(f"planar:           {pfw} @ {qpos[:6]}")
-                    with np.printoptions(precision=2):
-                        diff = pfw - site_world_pos
-                        print(f"planar - {watch_site}: {np.linalg.norm(diff):.3g} {diff}")
-                    print(f"qpos rot: {qpos[0]:.4g}, pitch {qpos[1]:.4g}, elbow {qpos[2]:.4g}, wrist_pitch {qpos[3]:.4g}, wrist_roll {qpos[4]:.4g}, jaw {qpos[5]:.4g}")
-                    for k, v in all.items():
-                        print(f"{k:16}: {v}")
+                    print(f"qpos rot: {qpos[0]:.4g}, pitch {qpos[1]:.4g}, elbow {qpos[2]:.4g}, wrist_pitch {qpos[3]:.4g}, wrist_roll {qpos[4]:.4g}, jaw {qpos[5]:.4g} ({qpos})")
+                    all_fk = d3.threed_fk(qpos, False)
+                    fkw = all_fk[watch]
+                    print(f"planar:           {fkw} @ {qpos[:6]}")
+                    for k, v in all_fk.items():
+                        fom = 0
+                        emphasis = ""
+                        if k in sites_xpos:
+                            delta = v - sites_xpos[k]
+                            fom = np.linalg.norm(delta, ord=np.inf)
+                            if fom > 0.001:
+                                emphasis = f"  {delta[0]:8.5f}, {delta[1]:8.5f}, {delta[2]:8.5f}"
+                        print(f"{k:16}: {v[0]:8.5f}, {v[1]:8.5f}, {v[2]:8.5f} {fom:0.1e}{emphasis}")
 
-                    qzero = np.zeros((6,))
-                    pfw_zero = d3.threed_fk(qzero, False)['jaw_grasp']
-                    print(f"planar zero {pfw_zero} @ {qzero[:4]}")
                 settled = True
 
-            site_prev = site_world_pos.copy()
-            fj_site_prev = fj_site_world_pos.copy()
+            watch_site_prev = watch_site_world_pos.copy()
+            alt_site_prev = alt_site_world_pos.copy()
             # body_id = model.body('target_body_name').id
             # current_pos = data.xpos[body_id]
 
